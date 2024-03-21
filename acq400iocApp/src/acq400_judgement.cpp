6dd26a67999544e3a97903b28bba8a758629d43d
Peter Milne <peter.milne@d-tacq.com>
2024-03-21 15:13:38 +0000

    Update from: ommit ada6bcb3d8b1259d4e06006b815e8f95171e3959 (HEAD -> main2) Author: Peter Milne <peter.milne@d-tacq.com> Date:   Thu Mar 21 15:11:39 2024 +0000
        const members ix_sc, ix_scc avoid repeat eval of ISACQ480()
         On branch main2
                modified:   ES.h
    
     On branch epics7
    	modified:   acq400iocApp/src/acq400_judgement.cpp

--- acq400iocApp/src/acq400_judgement.cpp
+++ acq400iocApp/src/acq400_judgement.cpp
@@ -225,8 +225,8 @@ int acq400Judgement::handle_es(unsigned* raw)
 		RESULT_FAIL[0] = burst_count;			// burst_count%256 .. maybe match to exact count and TS */
 		return 0;
 	}else if (ESX.isES(raw)){
-		sample_count = raw[IX_SC];
-		clock_count[1]= raw[IX_SCC];
+		sample_count = raw[ESX.ix_sc];
+		clock_count[1]= raw[ESX.ix_scc];
 		/** @@todo: not sure how to merge EPICS and SAMPLING timestamps.. go pure EPICS */
 		++burst_count;
 		RESULT_FAIL[0] = burst_count;			// burst_count%256 .. maybe match to exact count and TS */
