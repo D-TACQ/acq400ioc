3a76541906e085f60ac3b5f311d605a32d1c9dc4
Peter Milne <peter.milne@d-tacq.com>
2024-08-11 14:45:32 +0100

    intro AO422 	modified:   scripts/load.records 	modified:   scripts/mtype.sh

--- scripts/mtype.sh
+++ scripts/mtype.sh
@@ -29,6 +29,7 @@ MT_DIOB=67
 MT_AO420=40
 MT_AO424=41
 MT_AO4220=42
+MT_AO422=44
 MT_PIG_CELF=68
 MT_RAD_CELF=69
 MT_AO428=6A
@@ -59,28 +60,30 @@ nameFromMT() {
 	$MT_ACQ2106)	echo "acq2106";;
 	$MT_ACQ2206)	echo "acq2206";;
 	$MT_ACQ420|$MT_ACQ420F)
-					echo "acq420";;	
-	$MT_ACQ435)		echo "acq435";;
-	$MT_ACQ430)		echo "acq430";;
+			echo "acq420";;	
+	$MT_ACQ435)	echo "acq435";;
+	$MT_ACQ430)	echo "acq430";;
 	$MT_ACQ423)     echo "acq423";;
-	$MT_ACQ424)		echo "acq424";;
+	$MT_ACQ424)	echo "acq424";;
 	$MT_ACQ425|$MT_ACQ425F)		
-					echo "acq425";;	
+			echo "acq425";;	
 	$MT_ACQ427|$MT_ACQ427F)	
-					echo "acq427";;
-	$MT_ACQ436)		echo "acq436";;
-	$MT_ACQ437)		echo "acq437";;
-	$MT_ACQ465)             echo "acq465";;
-	$MT_ACQ480)		echo "acq480";;
-	$MT_BOLO8)		echo "bolo8";;
+			echo "acq427";;
+	$MT_ACQ436)	echo "acq436";;
+	$MT_ACQ437)	echo "acq437";;
+	$MT_ACQ465)     echo "acq465";;
+	$MT_ACQ480)	echo "acq480";;
+	$MT_BOLO8)	echo "bolo8";;
 	$MT_DIO432|$MT_DIO432P)
-					echo "dio432";;
-	$MT_DIO482)		echo "dio432";;
+			echo "dio432";;
+	$MT_DIO482)	echo "dio432";;
 	$MT_PMODAD1)	echo "pmodad1";;
-	$MT_BOLO8B)		echo "bolo8B";;
-	$MT_DIOB)		echo "diobiscuit";;
+	$MT_BOLO8B)	echo "bolo8B";;
+	$MT_DIOB)	echo "diobiscuit";;
 	$MT_AO420|$MT_AO4220)		
 	                echo "ao420";;
+	$MT_AO422)
+	                echo "ao422";;
 	$MT_AO424)		echo "ao424";;
 	$MT_PIG_CELF)	echo "pig-celf";;
 	$MT_RAD_CELF)	echo "rad-celf";;
@@ -88,8 +91,8 @@ nameFromMT() {
 	$MT_AO428)		echo "ao428elf";;
 	$MT_DIO482TD)   echo "dio482td";;
 	$MT_DIO482TD_PG) echo "dio482td_pg";;
-	$MT_ACQ494)    echo "acq494fmc";;
-	$MT_DIO422)    echo "dio422elf";;
+	$MT_ACQ494)     echo "acq494fmc";;
+	$MT_DIO422)     echo "dio422elf";;
 	$MT_DI460)      echo "di460elf";;
 	# no default	
 	esac					
@@ -104,7 +107,7 @@ case $(nameFromMT $1) in
 hasOutput() {
 	mt=$(echo $1 | sed -e s/^0//)
 	case $mt in
-	$MT_DIO432|$MT_DIO432P|$MT_DIO482TD|$MT_DIO482TD_PG|$MT_DIO482|$MT_AO420|$MT_AO4220|$MT_AO424|$MT_AO428|$MT_ACQ436|$MT_DIO422) echo "yes";;
+	$MT_DIO432|$MT_DIO432P|$MT_DIO482TD|$MT_DIO482TD_PG|$MT_DIO482|$MT_AO420|$MT_AO422|$MT_AO4220|$MT_AO424|$MT_AO428|$MT_ACQ436|$MT_DIO422) echo "yes";;
 	*) echo "no";;
 	esac
 }
