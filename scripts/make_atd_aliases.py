fce376506488eaf0a4bcd34cc53abe593cca85d1
Peter Milne <peter.milne@d-tacq.com>
2024-10-23 11:27:37 +0100

    rework to create aliases based on actual ATD records in set  On branch epics7 	modified:   scripts/make_atd_aliases.py

--- scripts/make_atd_aliases.py
+++ scripts/make_atd_aliases.py
@@ -4,31 +4,34 @@
 Generates aliases for analogue trigger
 
 Usage:
-    ./make_atd_aliases.py > /mnt/local/sysconfig/site-aliases.db
+    ./make_atd_aliases.py > /mnt/local/sysconfig/st_pre_atd-aliases.cmd
 """
 
 import os
 
 def run_main():
-    hostname = os.uname().nodename
-    agg = string_to_dict(cmd("get.site 0 aggregator"))
+    hostname, atd_sites = get_atd_sites()
 
-    for site in agg['sites'].split(','):
-        nchan = int(cmd(f"get.site {site} NCHAN"))
+    for site in atd_sites:
+        nchan = int(cmd(f"cat /etc/acq400/{site}/NCHAN"))
         for chan in range(1, nchan + 1):
             idk = "" if chan <= 16 else "2"
-            hex_val = f"B{get_cycled_value(chan) - 1:x}".upper()
-            print(f"alias( {hostname}:{site}:ANATRG:SRC{idk}.{hex_val}, {hostname}:{site}:ANATRG:LIVE:CH{chan:02} )")
-            print(f"alias( {hostname}:{site}:ANATRG:GROUP_MASK{idk}.{hex_val}, {hostname}:{site}:ANATRG:GROUP:CH{chan:02} )")
+            bit = f"B{(chan-1)%16:x}".upper()
+            print(f"alias( {hostname}:{site}:ANATRG:SRC{idk}.{bit}, {hostname}:{site}:ANATRG:LIVE:CH{chan:02} )")
+            print(f"alias( {hostname}:{site}:ANATRG:GROUP_MASK{idk}.{bit}, {hostname}:{site}:ANATRG:GROUP:CH{chan:02} )")
+
+def get_atd_sites():
+    atd_sites = []
+    hn = None
+    for pv in os.popen("grep ANATRG:GROUP_SRC$ /tmp/records.dbl").read().split("\n"):
+        fields = pv.split(':')
+        if len(fields) > 2:
+            atd_sites.append(fields[1])
+            hn = fields[0]
+    return hn, atd_sites
 
 def cmd(cmd):
     return os.popen(cmd).read()
-
-def string_to_dict(string):
-    return {key: value for key, value in (item.split('=') for item in string.split())}
-
-def get_cycled_value(n, max_value=16):
-    return (n - 1) % max_value + 1
 
 if __name__ == '__main__':
     run_main()
